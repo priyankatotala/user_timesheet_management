a6e5f83e7c7e76d2a4acff5057341b602bcc6720
priyankatotala <priyankatotalav@gmail..com>
2020-04-20 21:12:54 +1000

    Fixed the Styles and login

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
 
 
 Route::get('/home', 'HomeController@index')->name('home');
+
 
 
 Route::group(['middleware' => ['auth']], function() {
